59b4a771ee7bb853e176cd24bff6633fd6375cbd
Shashank <kshashank1122@gmail.com>
2015-01-29 00:15:28 +0530

    completed exit,pwd

--- ass2/myshell.c
+++ ass2/myshell.c
@@ -14,11 +14,18 @@ int main(int argc, char *argv[])
   }
   int length;
   while(1){
-    printf("%s>",cwd);
+    printf("$%s>",cwd);
     fgets(input,50,stdin);
     length=strlen(input);
     if(input[length-1]=='\n')
       input[length-1]='\0';
+    char cmnd[50];
+    sscanf(input,"%s",cmnd);
+    if(strcmp(cmnd,"exit")==0)break;
+    else if(strcmp(cmnd,"pwd")==0){
+      printf("%s\n",cwd);
+    }
+    //if(input[0]=='c'&&input[1]=='d')
     /*printf("%s",input);*/
     /*scanf("%s",input);*/
   }
