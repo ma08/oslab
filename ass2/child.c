c7edf4046b84b4b0b197526362cef24ee55f1793
Shashank <kshashank1122@gmail.com>
2015-02-05 16:17:21 +0530

    convert argv[i] to int in child.c

--- ass2/child.c
+++ ass2/child.c
@@ -5,9 +5,12 @@ int main(int argc, char const *argv[])
 {
 	int i;
 	freopen("childoutput","w",stdout);
-	for (i = 0; i < argc; ++i)
+	int pipe_value[2];
+	pipe_value[0]=strtol(argv[1],NULL,10);
+	pipe_value[1]=strtol(argv[2],NULL,10);
+	for (i = 0; i < 2; ++i)
 	{
-		printf("%s\n",argv[i]);
+		printf("%d\n",pipe_value[i]);
 	}
 	return 0;
 }
