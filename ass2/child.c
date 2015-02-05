df6c86267ec4d5f808f3196209aac9557dd6060d
mao8 <thisisma08@gmail.com>
2015-02-05 17:55:22 +0530

    Code in bad state

--- ass2/child.c
+++ ass2/child.c
@@ -14,14 +14,23 @@ int main(int argc, char const *argv[])
 	int pipe_value[2];
 	pipe_value[0]=strtol(argv[1],NULL,10);
 	pipe_value[1]=strtol(argv[2],NULL,10);
-  /*for (i = 0; i < 2; ++i)
+  
+  close(pipe_value[1]);
+  nbytes = read(pipe_value[0], buf, BSIZE);   
+  close(pipe_value[0]);
+
+  strcat(outputfilename,buf);
+  freopen(outputfilename,"w",stdout);
+  for (i = 0; i < 2; ++i)
   {
     printf("%d\n",pipe_value[i]);
-  }*/
-  nbytes = read(pipe_value[0], buf, BSIZE);   
-  strcat(outputfilename,buf);
-	freopen(outputfilename,"w",stdout);
-	id=strtol(buf,NULL,10);
-  printf("%d\n",id);
+  }
+  id=strtol(buf,NULL,10);
+  /*printf("%d\n",id);*/
+  sprintf(buf,"%d",1000);
+  if(write(pipe_value[1], buf, BSIZE)<=0)
+    perror("hoo"); 
+  close(pipe_value[1]);
+
 	return 0;
 }
--- ass2/parent.c
+++ ass2/parent.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
   int **pipe_ids;
   char buf[100];
+  char buf2[100];
   pid_t pid=-1;
   pid_t child_pids[5];
   char a[10];
@@ -16,10 +17,13 @@ int main(int argc, char *argv[])
   cmd[3]=(char *)0;
   for(i=0;i<5;++i)
     pipe_ids[i]=(int*)(malloc(sizeof(int)*2));
-  
   for (i = 0; i < 5; ++i)
   {
     pipe(pipe_ids[i]);
+    /*printf("\n%d %d\n",pipe_ids[i][0],pipe_ids[i][1]);*/
+  }
+  for (i = 0; i < 1; ++i)
+  {
     pid=fork();
     child_pids[i]=pid;
     sprintf(a,"%d",pipe_ids[i][0]);
@@ -28,11 +32,19 @@ int main(int argc, char *argv[])
     cmd[2]=b;
     if(pid==0){
       execvp("./child.out",cmd);
-      perror("");
+      /*perror("");*/
     }else{
       sprintf(buf,"%d",i);
+      close(pipe_ids[i][0]);
       write(pipe_ids[i][1], buf, 5); 
+      close(pipe_ids[i][1]);
     }
+  }
+  for(i=0; i<1 ;++i){
+    close(pipe_ids[i][1]);
+    read(pipe_ids[i][0],buf2,100);
+    close(pipe_ids[i][0]);
+    /*printf("%s",buf2);*/
   }
   return 0;
 }
