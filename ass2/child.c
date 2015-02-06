15e601ca2a6bba65ccfb95c8699a243f20927432
Shashank <kshashank1122@gmail.com>
2015-02-07 01:29:29 +0530

    Cleaned the code

--- ass2/child.c
+++ ass2/child.c
@@ -21,15 +21,13 @@ int drop_numbers(int **data, int n){
   int i,j=0;
   int *arr=*data;
   int count=0;
-  for (i = 0; i < n; ++i)
-  {
-    if(arr[i]!=N_INF)
-      count++;
+  for(i=0;i<n;++i)
+  {if(arr[i]!=N_INF)
+    count++;
   }
   int *final_arr=(int *)(malloc(sizeof(final_arr)*count));
-  for (i = 0; i < n; ++i)
-  {
-    if(arr[i]!=N_INF){
+  for(i=0;i<n;++i)
+  {if(arr[i]!=N_INF){
       final_arr[j++]=arr[i];
     }
   }
@@ -50,7 +48,6 @@ int main(int argc, char const *argv[])
   FILE *fp;
   int *data;
   data=(int *)(malloc(sizeof(int)*5));
-
   int size_data=0;
   const int BSIZE = 100;
   char buf[BSIZE];
@@ -58,129 +55,63 @@ int main(int argc, char const *argv[])
   char datafilename[20];
   strcpy(outputfilename,"childoutput");
   strcpy(datafilename,"data");
-	/*int pipe_value[2];*/
 	int read_end=strtol(argv[1],NULL,10);
 	int write_end=strtol(argv[2],NULL,10);
-  
-  /*close(write_end);*/
-  nbytes = read(read_end, buf, BSIZE);   
-  /*printf("--%s\n",buf);*/
-  /*perror("");*/
-  /*close(read_end);*/
-
+  nbytes = read(read_end, buf, BSIZE);
   strcat(outputfilename,buf);
   strcat(datafilename,buf);
   strcat(datafilename,".txt");
-  /*printf("\n---%s---\n",datafilename);*/
   freopen(outputfilename,"w",stdout);
-  /*printf("%d\n",read_end);*/
-  /*printf("%d\n",write_end);*/
-  /*for (i = 0; i < 2; ++i)
-  {
-    printf("%d\n",pipe_value[i]);
-  }*/
   id=strtol(buf,NULL,10);
-  /*printf("\n--%d\n",id);*/
-  /*printf("%d\n",id);*/
-  /*sprintf(buf,"%d",1000);*/
-  /*while(read(pipe_value[1],buf,BSIZE)<=0)perror("hoo");*/
-  /*if(write(pipe_value[1], buf, BSIZE)<=0)
-    perror("hoo"); */
-  /*nbytes = strlen(buf)+1;*/
-  /*close(read_end);*/
-  /*printf("----writing %d----",write_end);*/
-  /*write(write_end, buf, nbytes);*/
-  /*if(errno!=0)
-    perror("hoo");
-  close(write_end);*/
-
-  if((fp = fopen(datafilename, "r+")) != NULL) {
-    /*printf("wooooooooo");*/
-    /*perror(datafilename);*/
-    for (i = 0; i < 5; ++i)
-    {
-      ret=fscanf(fp, "%d", &num);
+  if((fp=fopen(datafilename,"r+"))!=NULL){
+    for(i=0;i<5;++i)
+    {ret=fscanf(fp, "%d", &num);
       if(ret!=EOF){
         size_data++;
         data[i]=num;
-      }else{
+      }
+      else{
         perror("Erroor");
       }
     }
-    /*for (i = 0; i < 5; ++i)
-      printf("%d ",data[i]);*/
-
-    /*sprintf(buf,"%d",READY);
-    nbytes = strlen(buf)+1;
-    write(write_end, buf, nbytes);*/
-
     write_to_parent(READY,write_end);
-    /*close(write_end);*/
     strcpy(buf,"oo");
-
-    /*for (j = 0; j < 2; ++j)*/
-    /*{*/
-      
-    /*}*/
     while(1){
-    /*if(id==2){*/
-
       errno=0;
-      nbytes = read(read_end, buf, 5);   
-      /*while(read(read_end, buf, 5)==0);*/
-      /*perror("");*/
-      /*printf("\n---%d--%s--\n",id,buf);*/
-      /*close(read_end);*/
+      nbytes = read(read_end, buf, 5);
       sig=strtol(buf,NULL,10);
-      switch (sig) {
+      switch(sig){
         case REQUEST:
           if(size_data>0){
-            
             while(data[(num=rand()%size_data)]==N_INF){
-              
             };
             write_to_parent(data[num],write_end);
-            /*sprintf(buf,"\n%d",data[rand()%size_data]);*/
           }else{
             write_to_parent(-1,write_end);
-            /*sprintf(buf,"\n-1");*/
           }
-          /*nbytes = strlen(buf)+1;*/
-          /*write(write_end, buf, nbytes);*/
-          /*printf("hoooooooooo");*/
           break;
         case PIVOT:
-          nbytes = read(read_end, buf, 5);   
-          /*printf("\n--%d-%s--\n",id,buf);*/
+          nbytes = read(read_end, buf, 5);
           sig=strtol(buf,NULL,10);
           pivot=sig;
-          /*printf("\npivot is %d\n",pivot);*/
           num_greater=0;
-          for (i = 0; i < size_data; ++i)
-          {
-            if(data[i]>pivot)
+          for(i=0;i<size_data;++i)
+          {if(data[i]>pivot)
               num_greater++;
-            
           }
-          /*sprintf(buf,"\n%d",num_greater);*/
-          /*nbytes = strlen(buf)+1;*/
-          /*write(write_end, buf, nbytes);*/
           write_to_parent(num_greater,write_end);
-          /*perror("");*/
           break;
         case SMALL:
-          for (i = size_data-1; i >=0; --i)
-          {
-            if(data[i]<pivot){
+          for(i=size_data-1;i>=0;--i)
+          {if(data[i]<pivot){
               data[i]=N_INF;
             }
           }
           size_data=drop_numbers(&data,size_data);
           break;
         case LARGE:
-          for (i = size_data-1; i >= 0; --i)
-          {
-            if(data[i]>pivot){
+          for (i=size_data-1;i>=0;--i)
+          {if(data[i]>pivot){
               data[i]=N_INF;
             }
           }
@@ -188,14 +119,12 @@ int main(int argc, char const *argv[])
           break;
         case EXIT:
           return 0;
-        /*default:*/
       }
-    /*}*/
     }
-  }else{
+  }
+  else{
     perror(datafilename);
     exit(1);
   }
-
 	return 0;
 }
--- ass2/parent.c
+++ ass2/parent.c
@@ -8,7 +8,7 @@
 
 int write_int_to_child(int childno,int sig,int **ptoc_ids){
   int nbytes;
-  char *buf=(char *)(malloc(sizeof(char)*20));
+  char *buf=(char*)(malloc(sizeof(char)*20));
   sprintf(buf,"%d",sig);
   nbytes=strlen(buf)+1;
   nbytes=write(ptoc_ids[childno][1], buf, nbytes); 
@@ -24,9 +24,8 @@ int write_str_to_child(int childno,char* buf,int **ptoc_ids){
 int write_to_all(char *buf,int **ptoc_ids){
   int i;
   int nbytes=strlen(buf)+1;
-  for (i = 0; i < 5; ++i)
-  {
-    nbytes=write(ptoc_ids[i][1], buf, nbytes); }
+  for(i=0;i<5;++i)
+  {nbytes=write(ptoc_ids[i][1], buf, nbytes); }
   return nbytes;
 }
 
@@ -57,17 +56,12 @@ int main(int argc, char *argv[])
     ptoc_ids[i]=(int*)(malloc(sizeof(int)*2));
     ctop_ids[i]=(int*)(malloc(sizeof(int)*2));
   } 
-  for (i = 0; i < 5; ++i)
-  {
-    pipe(ptoc_ids[i]);
+  for(i=0;i<5;++i)
+  {pipe(ptoc_ids[i]);
     pipe(ctop_ids[i]);
-    /*printf("\n%d %d",ptoc_ids[i][0],ptoc_ids[i][1]);*/
-    /*printf("\n%d %d",ctop_ids[i][0],ctop_ids[i][1]);*/
-    /*printf("\n%d %d\n",pipe_ids[i][0],pipe_ids[i][1]);*/
   }
-  for (i = 0; i < 5; ++i)
-  {
-    pid=fork();
+  for(i=0;i<5;++i)
+  {pid=fork();
     child_pids[i]=pid;
     sprintf(a,"%d",ptoc_ids[i][0]);
     sprintf(b,"%d",ctop_ids[i][1]);
@@ -75,46 +69,25 @@ int main(int argc, char *argv[])
     cmd[2]=b;
     if(pid==0){
       execvp("./child.out",cmd);
-      /*perror("");*/
-    }else{
+    }
+    else{
       write_int_to_child(i,i+1,ptoc_ids);
-      /*sprintf(buf,"%d",i+1);*/
-      /*nbytes=strlen(buf)+1;*/
-      /*close(ptoc_ids[i][0]);*/
-      /*printf("\n$$$%d\n",nbytes);*/
-      /*nbytes=write(ptoc_ids[i][1], buf, nbytes); */
-      /*printf("\n$$$%d\n",nbytes);*/
-      /*close(ptoc_ids[i][1]);*/
     }
   }
-  for(i=0; i<5 ;++i){
-    /*printf("----reading----");*/
+  for(i=0;i<5;++i){
     strcpy(buf2,"oo");
-    /*close(ctop_ids[i][1]);*/
     read(ctop_ids[i][0],buf2,sizeof(buf2));
     printf("Child %d sends READY\n",i+1);
     if(errno!=0)
       perror("");
-    /*close(ctop_ids[i][0]);*/
-    /*printf("\n%d %s",i,buf2);*/
   }
   printf("Parent READY\n");
   k=n/2;
   while(1){
     while(1){
       rand_child=rand()%5;
-      /*printf("---%d----",rand_child+1);*/
-      /*sprintf(buf,"%d",REQUEST);*/
-      /*nbytes=strlen(buf)+1;*/
-      /*close(ptoc_ids[rand_child][0]);*/
-      /*printf("----writing----");*/
-      /*nbytes=write(ptoc_ids[rand_child][1], buf, nbytes); */
-      /*printf("----%d---written---",nbytes);*/
-      /*perror("");*/
-
       write_int_to_child(rand_child,REQUEST,ptoc_ids);
       printf("Parent sends REQUEST to Child %d\n",rand_child+1);
-
       read(ctop_ids[rand_child][0],buf2,sizeof(buf2));
       sig=strtol(buf2,NULL,10);
       if(sig!=-1){
@@ -125,32 +98,16 @@ int main(int argc, char *argv[])
     }
     sprintf(buf,"%d",PIVOT);
     nbytes=write_to_all(buf,ptoc_ids);
-    /*nbytes=strlen(buf)+1;*/
-    /*for (i = 0; i < 5; ++i)
-    {
-      write_str_to_child(i,buf,ptoc_ids);
-      [>write(ptoc_ids[i][1], buf, nbytes); <]
-    }*/
     sprintf(buf,"%d",pivot);
     nbytes=write_to_all(buf,ptoc_ids);
     printf("Parent broadcasts pivot %d to all children\n",pivot);
-    /*nbytes=strlen(buf)+1;
-    for (i = 0; i < 5; ++i)
-    {
-      write_str_to_child(i,buf,ptoc_ids);
-      [>write(ptoc_ids[i][1], buf, nbytes); <]
-      [>perror("");<]
-    }*/
     sum=0;
     int sig[5];
-    for (i = 0; i < 5; ++i)
-    {
-      read(ctop_ids[i][0],buf2,sizeof(buf2));
+    for(i=0;i<5;++i)
+    {read(ctop_ids[i][0],buf2,sizeof(buf2));
       sig[i]=strtol(buf2,NULL,10);
-      /*for(j=0;j<5;j++)*/
       printf("Child %d receives pivot and replies %d\n",i+1,sig[i]);
       sum+=sig[i];
-      /*perror("");*/
     }
     printf("Parent: m=%d + %d + %d + %d + %d = %d. ",sig[0],sig[1],sig[2],sig[3],sig[4],sum);
     if(sum==k){
@@ -164,12 +121,14 @@ int main(int argc, char *argv[])
       nbytes=write_to_all(buf,ptoc_ids);
       printf("Parent terminates\n");
       return 0;
-    }else if(sum>k){
+    }
+    else if(sum>k){
       printf("Median not found. All elements smaller than pivot are removed by child processes.\n");
       printf("Process continued again. :(\n");
       sprintf(buf,"%d",SMALL);
       nbytes=write_to_all(buf,ptoc_ids);
-    }else if(sum<k){
+    }
+    else if(sum<k){
       printf("Median not found. All elements larger than pivot are removed by child processes.\n");
       printf("Process continued again. :(\n");
       sprintf(buf,"%d",LARGE);
@@ -177,7 +136,5 @@ int main(int argc, char *argv[])
       k=k-sum;
     }
   }
-  /*close(ptoc_ids[rand_child][1]);*/
-
   return 0;
 }
