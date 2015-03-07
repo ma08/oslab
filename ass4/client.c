643cad7111e111a26bbf0057f09b6212ee052303
mao8 <thisisma08@gmail.com>
2015-03-07 22:31:43 +0530

    Printing list in client

--- ass4/client.c
+++ ass4/client.c
@@ -8,6 +8,7 @@
 #include<errno.h>
 int main(int argc, char *argv[])
 {
+  char list[300];
   char a[50];
   printf("\nEnter id:");
   scanf("%s",a);
@@ -38,11 +39,31 @@ int main(int argc, char *argv[])
   }
   /*exit(1);*/
   printf("\n%d ",down_qid);
-  while(1){
-    while(msgrcv(down_qid,&get_msg,200,pid,MSG_NOERROR|IPC_NOWAIT)<0){
-      perror("receiving");
+  while(msgrcv(down_qid,&get_msg,200,pid,MSG_NOERROR|IPC_NOWAIT)<0){
+    perror("receiving");
+  }
+  if(get_msg.mtext[0]=='L'&&get_msg.mtext[1]=='I'){
+    strcpy(list,get_msg.mtext+5);
+  }
+  int i=1;
+  int j=0;
+  printf("\nList of clients");
+  printf("\n1. ");
+  i++;
+  while(list[j]!='\0'){
+    if(list[j]==' '){
+      printf("\n%d. ",i);
+      i++;
     }
-    printf("\n %s",get_msg.mtext);
+    else{
+      printf("%c",list[j]);
+    }
+    j++;
+  }
+  
+
+  while(1){
+    
     exit(1);
   }
   return 0;
