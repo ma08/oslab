9cf1110568e92ff285c043834b7ed2f8c5b00f8b
Shashank <kshashank1122@gmail.com>
2015-03-07 19:04:25 +0530

    added server.c,codes.h

--- ass4/codes.h
+++ ass4/codes.h
@@ -1,2 +1,8 @@
 #define UPQ 100
 #define DOWNQ 101
+
+struct mymsg{
+    long    mtype;     /* Message type. */
+    char    mtext[100];  /* Message text. */
+};
+typdef struct mymsg msg;
--- ass4/server.c
+++ ass4/server.c
@@ -19,6 +19,7 @@ int main(int argc, char const *argv[])
 		perror("msgget");
 		exit(0);
 	}
+	msg message;
 	while(1){
 		if(msgrcv())
 	}
