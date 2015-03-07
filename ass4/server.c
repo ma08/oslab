5fb6607846819e5c78d5261f07de042e44b353a8
Shashank <kshashank1122@gmail.com>
2015-03-07 19:02:33 +0530

    added server.c

---
+++ ass4/server.c
@@ -0,0 +1,26 @@
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "codes.h"
+
+int main(int argc, char const *argv[])
+{
+	int up_qid,down_qid;
+	struct msqid_qs qstat;
+	key_t up_key,down_key;
+	up_key=UPQ;
+	down_key=DOWNQ;
+	if((up_qid=msgget(up_key,IPC_CREAT|0666))<0){
+		perror("msgget");
+		exit(0);
+	}
+	if((down_qid=msgget(down_key,IPC_CREAT|0666))<0){
+		perror("msgget");
+		exit(0);
+	}
+	while(1){
+		if(msgrcv())
+	}
+	return 0;
+}
