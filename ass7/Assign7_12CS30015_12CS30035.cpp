be2395db548d6fee278e279ecfb99bcf8709fbef
Shashank <kshashank1122@gmail.com>
2015-04-13 21:04:52 +0530

    Final Look-2

--- ass7/Assign7_12CS30015_12CS30035.cpp
+++ ass7/Assign7_12CS30015_12CS30035.cpp
@@ -1,4 +1,6 @@
 #define _XOPEN_SOURCE 600
+#define NUMTHREADS 5
+#define LEVEL 3
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -9,8 +11,6 @@
 #include <queue>
 #include <curl/curl.h>
 #include <htmlcxx/html/ParserDom.h>
-#define NUMTHREADS 5
-#define LEVEL 3
 using namespace std;
 using namespace htmlcxx;
 
@@ -40,19 +40,15 @@ void level_increase(int id){
 
 void init_string(struct Struct *s){
   s->len=0;
-  s->ptr=(char*) malloc(s->len+1);
-  if(s->ptr==NULL){
-    exit(EXIT_FAILURE);
-  }
+  s->ptr=(char*)malloc(s->len+1);
+  if(s->ptr==NULL)exit(EXIT_FAILURE);
   s->ptr[0]='\0';
 }
 
 size_t writefunc(void *ptr, size_t size, size_t nmemb, struct Struct *s){
   size_t new_len=s->len+size*nmemb;
-  s->ptr=(char*)realloc(s->ptr, new_len+1);
-  if(s->ptr==NULL){
-    exit(EXIT_FAILURE);
-  }
+  s->ptr=(char*)realloc(s->ptr,new_len+1);
+  if(s->ptr==NULL)exit(EXIT_FAILURE);
   memcpy(s->ptr+s->len,ptr,size*nmemb);
   s->ptr[new_len]='\0';
   s->len=new_len;
@@ -89,8 +85,8 @@ void* parse_urls(void* threadid)
       struct Struct s;
       init_string(&s);
       curl_easy_setopt(curl,CURLOPT_URL,url.c_str());
-      curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION, writefunc);
-      curl_easy_setopt(curl,CURLOPT_WRITEDATA, &s);
+      curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,writefunc);
+      curl_easy_setopt(curl,CURLOPT_WRITEDATA,&s);
       res=curl_easy_perform(curl);
       string html(s.ptr);
       free(s.ptr);
@@ -119,29 +115,23 @@ int main(int argc, char const *argv[])
   pthread_t threads[NUMTHREADS];
   pthread_mutex_init(&to_do_next_mutex,NULL);
   pthread_mutex_init(&to_do_mutex,NULL);
-  int ret1,ret2,x; 
-  ret1=pthread_barrier_init(&barrier1,&attr,NUMTHREADS);
-  ret2=pthread_barrier_init(&barrier2,&attr,NUMTHREADS);
+  int x; 
+  pthread_barrier_init(&barrier1,&attr,NUMTHREADS);
+  pthread_barrier_init(&barrier2,&attr,NUMTHREADS);
   to_do.push("http://cse.iitkgp.ac.in/");
   void* status;
-  for(int i=0;i<NUMTHREADS;i++){
-    x=pthread_create(&threads[i],NULL,parse_urls,(void*)(i+1));
-  }
-  for(int i=0;i<NUMTHREADS;i++){
-    x=pthread_join(threads[i],&status);
-      if(x){
-        printf("ERROR; return code from pthread_join() is %d\n",x);
-        exit(-1);
-      }
-  }
-  cout<<"\n<URL>\t<Thread-id>\tLevel\n\n"
+  for(int i=0;i<NUMTHREADS;i++)x=pthread_create(&threads[i],NULL,parse_urls,(void*)(i+1));
+
+  for(int i=0;i<NUMTHREADS;i++)x=pthread_join(threads[i],&status);
+
+  cout<<"\n<URL>\t<Thread-id>\tLevel\n\n";
   for(int i=0;i<done.size();++i){
     cout<<done[i].first.first<<"\t"<<done[i].first.second<<"\t"<<done[i].second<<endl;
   }
   while(to_do.size()>0){
     string s=to_do.front();
     to_do.pop();
-    cout<<s<<"\t0\t"<<"\t"<<LEVEL<<endl;
+    cout<<s<<"\t0"<<"\t"<<LEVEL<<endl;
   }
   pthread_barrierattr_destroy(&attr);
   pthread_mutex_destroy(&to_do_mutex);
